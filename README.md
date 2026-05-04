7a4f1b9d792d151b6493f002b33e17d20fdb999c
HorseDev64 <horsedev64@gmail>
2026-05-04 16:10:55 -0500

    added high performance settings (hyprland)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # for_switching_pcs
+
+Install this packages for a low performance build
+-x11
+i3wm
+feh (background)
+tint2 (app bar)
+
+For high performance
+
+-wayland
+hyprland
+waybar
+rofi
+mpvpaper (animated background)
+
---
+++ high_performance/hypr/reload.sh
@@ -0,0 +1,13 @@
+killall waybar
+
+
+waybar &
+
+killall xremap-hypr-2
+
+xremap-hypr-2 ~/.config/xremap/config.yml &
+
+
+killall mpvpaper 
+
+mpvpaper -p -o "loop --hwdec=auto --no-audio" HDMI-A-1 ~/pictures/zelda_low_qual_wall.mp4
